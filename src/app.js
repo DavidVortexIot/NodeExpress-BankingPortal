e71f47ab2aa697854a38af42137e6fa46a7287d6
David Jones <david@vortexiot.com>
2018-10-09 10:35:00 +0100

    part 2

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.post('/transfer', function(req, res) {
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-    res.render('transfer', {message: 'Transfer Complete'});
+    res.render('transfer', {message: 'Transfer Completed'});
 });
 
 app.get('/payment', function (req, res) {
